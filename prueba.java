f1426b2804dc31e1ccb4aad8acc49d06a5102782
Maria De Jesus Rios Cruz <maria.rios@elektra.com.mx>
2022-05-18 11:53:23 -0500

    Versión 1 de los archivos

---
+++ prueba.java
@@ -0,0 +1,8 @@
+public class CjAppConfig extends CjApplicationInit {
+    public static final String BASE_PACKAGE_FIDUCIARIO = "com.baz.fiduciario";
+    public static final String PROPERTIES_APPLICATION  = "classpath:/com/baz/fiduciario/properties/application.properties";
+    public static final String PROPERTIES_MESSAGES     = "classpath:/com/baz/fiduciario/properties/messages.properties";
+    public static final String BEAN_ADMIN_NOTIFIER     = "adminNotifier";
+    public static final String BEAN_USER_NOTIFIER = "userNotifier";
+    public static final String BEAN_EMAIL_SENDER_FACTORY = "emailSenderFactory";
+}
